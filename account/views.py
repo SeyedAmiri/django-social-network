7cfaf322666bba048f7a05ebcb2ad45898588bca
SeyedAmiri <amiridev81@gmail.com>
2024-05-17 13:45:44 +0330

    Password and confirm password validation with clean method

--- account/views.py
+++ account/views.py
@@ -15,9 +15,10 @@ class RegisterView(View):
 
 	def post(self, request):
 		form = self.form_class(request.POST)
+
 		if form.is_valid():
 			cd = form.cleaned_data
-			User.objects.create_user(cd['username'], cd['email'], cd['password'])
+			User.objects.create_user(cd['username'], cd['email'], cd['password1'])
 			messages.success(request, 'you registered successfully', 'success')
 			return redirect('home:home')
 		return render(request, self.template_name, {'form': form})
